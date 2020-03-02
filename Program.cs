35259e92abb785532e44f1873bd1cedceb376342
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-03-02 23:39:56 +0200

    wip...

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using Statiq.App;
+using Statiq.Web;
+using System.Threading.Tasks;
+
+namespace Blog
+{
+    class Program
+    {
+        public static async Task<int> Main(string[] args) =>
+            await Bootstrapper.Factory
+                .CreateWeb(args)
+                .RunAsync();
+    }
+}
