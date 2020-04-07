e369acb4a17dbe7c1995e923d5e47973033dbc9b
Martin Björkström <martin.bjorkstrom@gmail.com>
2020-04-08 00:34:17 +0300

    Moar...

--- src/Program.cs
+++ src/Program.cs
@@ -5,6 +5,7 @@ using Statiq.App;
 using Statiq.Common;
 using Statiq.Core;
 using Statiq.Handlebars;
+using Statiq.Html;
 using Statiq.Markdown;
 using Statiq.Yaml;
 
@@ -34,7 +35,31 @@ namespace site
             {
                 new ExtractFrontMatter(new ParseYaml()),
                 new RenderMarkdown(),
+                new GenerateExcerpt(),
                 new SetDestination(".html")
+            };
+
+            PostProcessModules = new ModuleList
+            {
+                new SetMetadata("template", Config.FromContext(async ctx => await ctx.Outputs
+                    .FromPipeline(nameof(LayoutPipeline))
+                    .First(x => x.Source.FileName == "layout.hbs")
+                    .GetContentStringAsync())),
+                new RenderHandlebars("template")
+                    .Configure(async (context, document, handlebars) =>
+                    {
+                        foreach (var partial in context.Outputs
+                            .FromPipeline(nameof(LayoutPipeline)).WhereContainsKey("partial"))
+                        {
+                            handlebars.RegisterTemplate(
+                                partial.GetString("partial"),
+                                await partial.GetContentStringAsync());
+                        }
+                    }).WithModel(Config.FromDocument(async doc => new
+                    {
+                        body = await doc.GetContentStringAsync()
+                    })),
+                new SetContent(Config.FromDocument(x => x.GetString("template")))
             };
 
             OutputModules = new ModuleList
@@ -75,6 +100,22 @@ namespace site
             OutputModules = new ModuleList
             {
                 new WriteFiles()
+            };
+        }
+    }
+    
+    public class LayoutPipeline : Pipeline
+    {
+        public LayoutPipeline()
+        {
+            InputModules = new ModuleList
+            {
+                new ReadFiles("{!_,}*.hbs")
+            };
+
+            ProcessModules = new ModuleList
+            {
+                new ExtractFrontMatter(new ParseYaml())
             };
         }
     }
@@ -169,6 +210,71 @@ namespace site
             };
         }
     }
+    
+    public class IndexPipeline : Pipeline
+    {
+        public IndexPipeline()
+        {
+            Dependencies.AddRange(nameof(BlogPostPipeline), nameof(TagsPipeline));
+
+            InputModules = new ModuleList
+            {
+                new ReadFiles("_index.hbs")
+            };
+
+            ProcessModules = new ModuleList
+            {
+                new ExtractFrontMatter(new ParseYaml()),
+                new SetDestination(Config.FromValue(new NormalizedPath("./index.html"))),
+                new RenderHandlebars()
+                    .WithModel(Config.FromDocument((doc, context) => new
+                    {
+                        description = doc.GetString("description"),
+                        intro = doc.GetString("intro"),
+                        posts = context.Outputs.FromPipeline(nameof(BlogPostPipeline))
+                            .OrderByDescending(x => x.GetDateTime("Published"))
+                            .Take(3)
+                            .Select(post => new
+                            {
+                                link = context.GetLink(post),
+                                title = post.GetString(Keys.Title),
+                                excerpt = post.GetString("Excerpt"),
+                                date = post.GetDateTime("Published").ToLongDateString()
+                            }),
+                        olderPosts = context.Outputs.FromPipeline(nameof(BlogPostPipeline))
+                            .OrderByDescending(x => x.GetDateTime("Published"))
+                            .Skip(3)
+                            .Select(post => new
+                            {
+                                link = context.GetLink(post),
+                                title = post.GetString(Keys.Title),
+                            }), 
+                        tags = context.Outputs.FromPipeline(nameof(TagsPipeline))
+                            .OrderByDescending(x => x.GetChildren().Length)
+                            .ThenBy(x => x.GetString(Keys.GroupKey))
+                            .Take(10)
+                            .Select(tag => new
+                            {
+                                link = context.GetLink(tag),
+                                title = tag.GetString(Keys.GroupKey),
+                                count = tag.GetChildren().Length
+                            }),
+                        socialMediaLinks = doc.GetChildren("socialMediaLinks")
+                            .Select(socialMediaLink => new
+                            {
+                                link = socialMediaLink.GetString("link"),
+                                @class = socialMediaLink.GetString("class"),
+                                title = socialMediaLink.GetString("title")
+                            })
+                    }))
+            };
+
+            OutputModules = new ModuleList
+            {
+                new WriteFiles()
+            };
+        }
+    }
 
     public class AssetsPipeline : Pipeline
     {
@@ -177,7 +283,7 @@ namespace site
             Isolated = true;
             ProcessModules = new ModuleList
             {
-                new CopyFiles("./assets/{css,fonts,js}/**/*.*")
+                new CopyFiles("./assets/{css,fonts,js}/**/*")
             };
         }
     }
